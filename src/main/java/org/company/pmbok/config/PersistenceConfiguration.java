6b6efab320227d9ba7356bbdb04166c5fa7d4088
Jose Olivares <shoutmetal@gmail.com>
2017-03-14 22:25:53 -0500

    start pmbok project

---
+++ src/main/java/org/company/pmbok/config/PersistenceConfiguration.java
@@ -0,0 +1,17 @@
+package org.company.pmbok.config;
+
+
+//@Configuration
+public class PersistenceConfiguration {
+
+	/*
+	@Bean
+	@ConfigurationProperties(prefix="spring.datasource")
+	@Primary
+	public DataSource dataSource()
+	{
+		return DataSourceBuilder.create().build();
+		
+	}
+	*/
+}
---
+++ src/main/java/org/company/pmbok/controller/HomeController.java
@@ -0,0 +1,18 @@
+package org.company.pmbok.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/home")
+public class HomeController {
+
+	@CrossOrigin
+	@RequestMapping("/greetings")
+	public String Index(){
+
+		return "Hello World!";
+	}
+	
+}
